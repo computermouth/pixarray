5a4c04ab808d8b2eaa04238fb7892cb9507bd366
computermouth <computermouth@crunchbangplusplus.org>
2017-06-01 20:34:27 -0700

    Set pixel format, and draw lines

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 
 all:
 	gcc main.c -o main -lSDL2
+
+memtest:
+	valgrind --track-origins=yes --leak-check=yes --show-reachable=yes ./main
--- main.c
+++ main.c
@@ -6,10 +6,10 @@
 #include <SDL2/SDL.h>
 
 typedef void* window_t;
-typedef unsigned int* pixel_buffer_t;
-typedef unsigned char rgba_t[4];
+typedef unsigned char* pixel_buffer_t;
+typedef unsigned char rgba_t[3];
 
-struct window_s{
+typedef struct{
 	SDL_Window* sdl_window;
 	SDL_Surface* sdl_surface;
 	SDL_Renderer* sdl_renderer;
@@ -18,11 +18,11 @@ struct window_s{
 	int height;
 	int pitch;
 	int received_quit_event;
-};
+} window_s;
 
 int window_create(window_t *window, char* title, int width, int height) {
 
-	struct window_s *window_p = (struct window_s*) calloc( 1, sizeof(struct window_s) );
+	window_s *window_p = ( window_s* ) calloc( 1, sizeof( window_s ) );
 
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
@@ -47,7 +47,7 @@ int window_create(window_t *window, char* title, int width, int height) {
 	}
 
 	window_p->sdl_texture = SDL_CreateTexture( window_p->sdl_renderer,
-		SDL_GetWindowPixelFormat( window_p->sdl_window ),
+		SDL_PIXELFORMAT_BGR888,
 		SDL_TEXTUREACCESS_STREAMING,
 		width, height );
 
@@ -69,7 +69,7 @@ int window_destroy(window_t window) {
 		return -1;
 	}
 	
-	struct window_s *window_p = (struct window_s*) window;
+	window_s *window_p = (window_s*) window;
 
 	if(window_p->sdl_texture) SDL_DestroyTexture( window_p->sdl_texture );
 	if(window_p->sdl_renderer) SDL_DestroyRenderer( window_p->sdl_renderer );
@@ -85,7 +85,7 @@ int window_update_events(window_t window) {
 		return -1;
 	}
 
-	struct window_s *window_p = (struct window_s*) window;
+	window_s *window_p = (window_s*) window;
 
 	SDL_Event event;
 	while(SDL_PollEvent(&event)) {
@@ -104,7 +104,7 @@ int window_received_quit_event(window_t window) {
 		return -1;
 	}
 
-	struct window_s *window_p = (struct window_s*) window;
+	window_s *window_p = (window_s*) window;
 	return window_p->received_quit_event;
 }
 
@@ -112,7 +112,7 @@ int window_update_buffer(window_t window, pixel_buffer_t pixels) {
 	if(!window) {
 		return -1;
 	}
-	struct window_s *window_p = (struct window_s*) window;
+	window_s *window_p = (window_s*) window;
 
 	void *texture_pixels = NULL;
 	int pitch = 0;
@@ -130,21 +130,38 @@ int window_update_buffer(window_t window, pixel_buffer_t pixels) {
 	return 0;
 }
 
+void memset_pixel( void * dest, void * value, size_t size ){
+	for (uint i = 0; i < size; i++)
+		memcpy(dest+(i*4), value, 3);
+}
+
 int main( void ) {
 	window_t window = NULL;
 	
 	pixel_buffer_t buffer = calloc(640 * 480, sizeof(int));
-	memset(buffer, 0xff, 640*480*4);
-	memset(buffer, 0x88, 640*480*2);
-
+	
+	rgba_t pink = { 0xa5, 0x54, 0x90 };
+	memset_pixel(buffer, &pink, (640*480));
+	
+	rgba_t blue = { 0x00, 0x54, 0x90 };
+	memset_pixel(buffer, &blue, (640*240));
+	
+	rgba_t green = { 0x00, 0xff, 0x90 };
+	memset_pixel(buffer + (640 * 155 * 4), &green, (640*1));
+	memset_pixel(buffer + (640 * 175 * 4), &green, (640*2));
+	memset_pixel(buffer + (640 * 195 * 4), &green, (640*4));
+	memset_pixel(buffer + (640 * 215 * 4), &green, (640*8));
+	
 	if(window_create(&window, "Testing", 640, 480)) {
 		printf("Failure\n");
 		return 1;
 	}
+	
 	while(!window_received_quit_event(window)) {
 		window_update_events(window);
 		window_update_buffer(window, buffer);
 	}
+	
 	window_destroy(window);
 	return 0;
 }
