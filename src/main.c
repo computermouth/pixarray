b301a1cd9d82047c5954a65ec6f5c23aac470be2
computermouth <computermouth@crunchbangplusplus.org>
2018-03-08 20:22:18 -0800

    Added cat_walk_left/right

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 
 #include "mario_sprite.h"
 #include "loading_sprite.h"
+#include "cat_sprite.h"
 
 int main( int argc, char * argv[] ) {
 	
@@ -15,27 +16,40 @@ int main( int argc, char * argv[] ) {
 	
 	init_mario_sprite();
 	init_loading_sprite();
+	init_cat_sprite();
 	
 	mario->pad_x = -80;
 	mario->pad_y = 595;
 	mario->active_animation = 3;
 	
+	cat->pad_x = 0;
+	cat->pad_y = 0;
+	cat->active_animation = 1;
+	
 	while(!ww_window_received_quit_event()) {
 		
 		ww_window_update_events();
 		
-		ww_draw_sprite(loading);
+		//~ ww_draw_sprite(loading);
 		
 		mario->pad_x += 5;
 		ww_draw_sprite(mario);
+		
+		//~ cat->pad_x += 2;
+		ww_draw_sprite(cat);
 		
 		ww_window_update_buffer();
 		
 		if (mario->pad_x > 1320)
 			mario->pad_x = -80;
+		
+		//~ if (cat->pad_x > 1320)
+			//~ cat->pad_x = -80;
 	}
 	
 	ww_free_sprite(mario);
+	ww_free_sprite(loading);
+	ww_free_sprite(cat);
 		
 	ww_window_destroy();
 	return 0;
